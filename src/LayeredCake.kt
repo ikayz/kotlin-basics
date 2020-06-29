cd505a3e13aec1062c230be5a7273290d3121fe2
Isaac Miti <ikayz@live.com>
2020-06-29 20:19:57 +0200

    Call two functions

--- src/LayeredCake.kt
+++ src/LayeredCake.kt
@@ -1,3 +1,17 @@
 /*
 Created by ikayz on 29/06/2020
 */
+fun main() {
+    val age = 24
+    val layers = 5
+    printCakeCandles(age)
+    printCakeTop()
+}
+
+fun printCakeTop() {
+    TODO("Not yet implemented")
+}
+
+fun printCakeCandles() {
+    TODO("Not yet implemented")
+}
