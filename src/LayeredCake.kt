61c50dd6aae21e84c7ac1fe36065583cfce44b50
Isaac Miti <ikayz@live.com>
2020-06-29 20:50:09 +0200

    Add printCakeTop code block

--- src/LayeredCake.kt
+++ src/LayeredCake.kt
@@ -4,17 +4,20 @@ Created by ikayz on 29/06/2020
 fun main() {
     val age = 24
     val layers = 5
-    printCakeCandles(age)
+//    printCakeCandles(age)
     printCakeTop(age)
-    printCakeBottom(age, layers)
+//    printCakeBottom(age, layers)
 }
 
 fun printCakeBottom(age: Int, layers: Int) {
     TODO("Not yet implemented")
 }
 
-fun printCakeTop() {
-    TODO("Not yet implemented")
+fun printCakeTop(age: Int) {
+    repeat(age + 2) {
+        print("=")
+    }
+    println()
 }
 
 fun printCakeCandles() {
