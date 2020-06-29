92304009bffebec882245db2b4b184365b27554c
Isaac Miti <ikayz@live.com>
2020-06-29 16:55:16 +0200

    Initial commit

---
+++ src/BirthdayBanner.kt
@@ -0,0 +1,15 @@
+/*
+Created by ikayz on 29/06/2020
+*/
+fun main() {
+    printBorder()
+    println("Happy Birthday Isaac!")
+    printBorder()
+}
+
+fun printBorder() {
+    repeat(23) {
+        print("=")
+    }
+    println()
+}
