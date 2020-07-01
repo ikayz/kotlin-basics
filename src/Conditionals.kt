f7f683e33534d2060a113e5f2c3e222f756664f8
Isaac Miti <ikayz@live.com>
2020-07-01 09:11:10 +0200

    Rewrite conditional using conditional expression

--- src/Conditionals.kt
+++ src/Conditionals.kt
@@ -2,10 +2,22 @@
 Created by ikayz on 01/07/2020
 */
 fun main() {
-    val count = 23
+    val count = 32
     if(count == 23) {
         println("That's the answer")
+    } else if (count == 32){
+        println("That's not quite the answer")
     } else {
         println("That's not the answer")
     }
+
+    // Conditional expression
+    val answerString: String = if (count == 42) {
+        "That's the answer."
+    } else if (count == 32) {
+        "The answer is not quite right."
+    } else {
+        "You got it wrong"
+    }
+    println(answerString)
 }
