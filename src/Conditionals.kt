ec3a9c0657519614f9d3fcac77b63551f96cc9df
Isaac Miti <ikayz@live.com>
2020-07-01 09:03:25 +0200

    Add conditional

---
+++ src/Conditionals.kt
@@ -0,0 +1,11 @@
+/*
+Created by ikayz on 01/07/2020
+*/
+fun main() {
+    val count = 23
+    if(count == 23) {
+        println("That's the answer")
+    } else {
+        println("That's not the answer")
+    }
+}
