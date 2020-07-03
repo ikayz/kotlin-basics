8273db9f8eb491b46118bc9ff7e10a74382c3353
Isaac Miti <ikayz@live.com>
2020-07-03 12:15:13 +0200

    Add function call and the count variable to the main function

--- src/Functions.kt
+++ src/Functions.kt
@@ -1,7 +1,10 @@
 /*
 Created by ikayz on 02/07/2020
 */
+
+// Function without an argument
 fun generateAnswerString(): String {
+    val count = 42
     val answerString = if (count == 42) {
         "I have the answer."
     } else {
@@ -11,6 +14,19 @@ fun generateAnswerString(): String {
     return answerString
 }
 
+// Function which takes an argument
+fun generateAnswerString(countThreshold: Int): String {
+    val count = 42
+    val answerString = if (count > countThreshold) {
+        "I have the answer."
+    } else {
+        "The answer eludes me."
+    }
+
+    return answerString
+}
+
 fun main() {
-    val answerString = generateAnswerString()
+    val answerString = generateAnswerString(42)
+    println(answerString)
 }
