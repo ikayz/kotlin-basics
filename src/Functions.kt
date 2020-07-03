9b946d12e0d28f230233d71c9a26fb59b7fcf76e
Isaac Miti <ikayz@live.com>
2020-07-03 12:21:40 +0200

    Simplify the function by returning direct

--- src/Functions.kt
+++ src/Functions.kt
@@ -1,7 +1,7 @@
 /*
 Created by ikayz on 02/07/2020
 */
-
+val count = 42
 // Function without an argument
 fun generateAnswerString(): String {
     val count = 42
@@ -15,18 +15,27 @@ fun generateAnswerString(): String {
 }
 
 // Function which takes an argument
+//fun generateAnswerString(countThreshold: Int): String {
+//    val count = 42
+//    val answerString = if (count > countThreshold) {
+//        "I have the answer."
+//    } else {
+//        "The answer eludes me."
+//    }
+//
+//    return answerString
+//}
+
+// Simplifying functions
 fun generateAnswerString(countThreshold: Int): String {
-    val count = 42
-    val answerString = if (count > countThreshold) {
+    return if (count > countThreshold) {
         "I have the answer."
     } else {
         "The answer eludes me."
     }
-
-    return answerString
 }
 
 fun main() {
-    val answerString = generateAnswerString(42)
+    val answerString = generateAnswerString(40)
     println(answerString)
 }
